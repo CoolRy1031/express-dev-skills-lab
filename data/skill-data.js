f7715043eabf5f6c48e8e44007b7d9344a61c05c
CoolRy <that007guy@hotmail.com>
2022-06-15 19:10:43 -0400

    add new skill button

--- data/skill-data.js
+++
@@ -1,9 +0,0 @@
-const skills = [
-  {text: 'Creativity', done: true, _id: 125223},
-  {text: 'Learn JavaScript', done: false, _id: 127904},
-  {text: 'Work as a team', done: false, _id: 139608},
-]
-
-export { 
-	skills
-}
