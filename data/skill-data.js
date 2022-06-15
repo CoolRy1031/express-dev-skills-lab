b4880fb00ae3f4c5c57287436c706a33b7171bda
CoolRy <that007guy@hotmail.com>
2022-06-15 17:21:21 -0400

    finish part 1

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {text: 'Creativity', done: true, _id: 125223},
+  {text: 'Learn JavaScript', done: false, _id: 127904},
+  {text: 'Work as a team', done: false, _id: 139608},
+]
+
+export { 
+	skills
+}
